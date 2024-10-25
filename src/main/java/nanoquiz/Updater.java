5e35267a42315045ab7a06f8d73db4f046455e49
cat24a <artur.2010@icloud.com>
2024-10-25 16:42:01 +0200

    bump version to 24w43b

--- src/main/java/nanoquiz/Updater.java
+++ src/main/java/nanoquiz/Updater.java
@@ -16,7 +16,7 @@ import org.json.JSONObject;
 
 public abstract class Updater {
     static final String updatesApiUriAsString = "https://api.github.com/repos/cat24a/nanoquiz/releases/latest";
-    public static final String VERSION = "24w43a";
+    public static final String VERSION = "24w43b";
     static final String updateDownloadUrl = "https://github.com/cat24a/nanoquiz";
 
     public static void checkForUpdates() {
